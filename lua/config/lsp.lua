f75bf3729a6ddeaa4c43f8a3d445028ddaaaaf98
sanghyeon <boh001g@gmail.com>
2025-06-18 23:10:27 +0700

    update

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,7 +1,6 @@
 -- lua
 vim.lsp.config["lua_ls"] = {
   cmd = { "lua-language-server" },
-  filetypes = { "lua" },
   root_markers = { ".git" },
   settings = {
     Lua = {
@@ -24,18 +23,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set(
-      "n",
-      "<leader>a",
-      '<cmd>lua require("fastaction").code_action()<CR>',
-      { buffer = ev.buf }
-    )
-    vim.keymap.set(
-      "v",
-      "<leader>a",
-      "<esc><cmd>lua require('fastaction').range_code_action()<CR>",
-      { buffer = ev.buf }
-    )
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
     vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
