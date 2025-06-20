7f9a74591b52611ef4bfc36a86650ddb1c225996
sanghyeon <boh001g@gmail.com>
2025-06-20 15:46:10 +0700

    update

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -32,6 +32,35 @@ return {
         find_command = { "fd", "-t=f", "-a" },
         path_display = { "absolute" },
         wrap_results = true,
+        preview = {
+          mime_hook = function(filepath, bufnr, opts)
+            local is_image = function(filepath)
+              local image_extensions = { "png", "jpg", "svg", "gif"} -- Supported image formats
+              local split_path = vim.split(filepath:lower(), ".", { plain = true })
+              local extension = split_path[#split_path]
+              return vim.tbl_contains(image_extensions, extension)
+            end
+            if is_image(filepath) then
+              local term = vim.api.nvim_open_term(bufnr, {})
+              local function send_output(_, data, _)
+                for _, d in ipairs(data) do
+                  vim.api.nvim_chan_send(term, d .. "\r\n")
+                end
+              end
+              vim.fn.jobstart({
+                "timg",
+                "-pk",
+                filepath, -- Terminal image viewer command
+              }, { on_stdout = send_output, stdout_buffered = true, pty = true })
+            else
+              require("telescope.previewers.utils").set_preview_message(
+                bufnr,
+                opts.winid,
+                "Binary cannot be previewed"
+              )
+            end
+          end,
+        },
       },
       extensions = {
         workspaces = {
@@ -49,6 +78,7 @@ return {
     telescope.load_extension("workspaces")
     telescope.load_extension("fzf")
     telescope.load_extension("yank_history")
+
 
     local builtin = require("telescope.builtin")
 
